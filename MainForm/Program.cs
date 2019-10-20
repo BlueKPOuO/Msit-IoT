c93a70cd494b189bb5c02775eb149f29c492430f
柯宜賢 <kocy50@gmail.com>
2019-10-20 17:51:08 +0800

    Test_20191020 1750
    解衝突練習測試

--- MainForm/Program.cs
+++ MainForm/Program.cs
@@ -17,7 +17,8 @@ namespace MainForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            //Application.Run(new MainForm());
+            Application.Run(new Form());
         }
     }
 }
